53ea640ee6fa14f94b30e662aa80baa51c755207
Seth <sethparkerpierce@icloud.com>
2022-10-31 18:52:07 -0400

    finished lab11

--- README.md
+++ README.md
@@ -4,21 +4,32 @@
 **Version**: 1.0.0
 
 ## Overview
-<!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) -->
+
+Application Display books from a database
 
 ## Getting Started
-<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+
+ Open the website and it will do the work itself
 
 ## Architecture
-<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+
+ MongoDB, Javascript, React
 
 ## Change Log
-<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:
 
-01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource. -->
+10/31/2022 3:10pm just starting to put inital parts into place
 
 ## Estimates
-<!-- See below -->
+
+Name of feature: Books
+
+Estimate of time needed to complete: 2 hourse
+
+Start time: 4 PM EST
+
+Finish time:  6:30 PM EST
+
+Actual time needed to complete: 2 and half hours
 
 ## Credit and Collaborations
 <!-- Give credit (and a link) to other people or resources that helped you build this application. -->
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel';
+import './Bestbook.css'
+import bookcover from './book.jpg';
+import Rating from './Rating';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -10,19 +15,48 @@ class BestBooks extends React.Component {
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
+getBooks = async () => {
+  try {
+    let bookData = await axios.get(`https://can-of-books-ste-sef.herokuapp.com/books`);
+    this.setState({books: bookData.data});
+  }catch(error){
+    console.log(`Error: ${error.response}`);
+  }
+}
+
+componentDidMount(){
+  this.getBooks();
+}
+
+
+
   render() {
-
+    console.log(this.state.books)
     /* TODO: render all the books in a Carousel */
-
+    let books = this.state.books.map(book => {
+      return <Carousel.Item key={book._id} id='carouselItem'>
+      <img
+        className="d-block w-100 h-50"
+        src={bookcover}
+        alt="First slide"
+      />
+      <Carousel.Caption>
+        <h3 id='bookTitle'>{book.title}</h3>
+        <p id='bookDescription'>{book.description}</p>
+        <Rating rating={book.rating} id='rating'/>
+      </Carousel.Caption>
+    </Carousel.Item>
+    })
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
+        <section id='bookSection'>
+          {this.state.books.length ? (
+            <div id='divContainer'><Carousel fade id='carousel'>{books}</Carousel></div>
+          ) : (
+            <h3>The Book Collection is Empty</h3>
+          )}
+        </section>
       </>
     )
   }
