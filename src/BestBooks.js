7079c571c5ea3cfe949e911802bac0d0b8c2bbbd
Seth <sethparkerpierce@icloud.com>
2022-11-01 16:59:51 -0400

    finished lab12

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -4,12 +4,15 @@ import Carousel from 'react-bootstrap/Carousel';
 import './Bestbook.css'
 import bookcover from './book.jpg';
 import Rating from './Rating';
+import Button from 'react-bootstrap/Button';
+import BookFormModal from './BookFormModal'
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      display: false
     }
   }
 
@@ -24,11 +27,50 @@ getBooks = async () => {
   }
 }
 
+handleBookSubmit = (event) => {
+  event.preventDefault();
+  let newBook = {
+    title: event.target.bookTtl.value,
+    description: event.target.bookDescript.value,
+    rating: event.target.bookRating.value
+  }
+  console.log(newBook)
+  this.postBook(newBook)
+}
+
+postBook = async (newBookObj) => {
+  try {
+    let newBook = await axios.post(`https://can-of-books-ste-sef.herokuapp.com/books` , newBookObj)
+
+    this.setState({
+      books: [...this.state.books, newBook.data],
+      display: false
+    })
+
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+deleteBook = async (id) => {
+  try {
+
+    await axios.delete(`https://can-of-books-ste-sef.herokuapp.com/books/${id}`)
+
+    let updatedBooks = this.state.books.filter(book => book._id !== id)
+
+    this.setState({
+      books: updatedBooks
+    })
+
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
 componentDidMount(){
   this.getBooks();
 }
-
-
 
   render() {
     console.log(this.state.books)
@@ -45,17 +87,20 @@ componentDidMount(){
         <p id='bookDescription'>{book.description}</p>
         <Rating rating={book.rating} id='rating'/>
       </Carousel.Caption>
+      <div id='buttonDiv'><Button onClick={() => {this.deleteBook(book._id)}} id='deleteBook' variant="danger">Delete Book</Button></div>
     </Carousel.Item>
     })
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+        <BookFormModal  show={this.state.display} onHide={()=> this.setState({display:false})} onSubmit={this.handleBookSubmit}/>
         <section id='bookSection'>
           {this.state.books.length ? (
             <div id='divContainer'><Carousel fade id='carousel'>{books}</Carousel></div>
           ) : (
             <h3>The Book Collection is Empty</h3>
           )}
+          <Button onClick={()=> this.setState({display:true})} id='addBook'>Add Book</Button>
         </section>
       </>
     )
